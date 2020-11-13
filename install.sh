9d682cbac24730c1766bc62bed6bf82a0d92c944
zamboni86 <zanoni.contreras@gmail.com>
2020-11-13 08:23:47 -0700

    change install script to AWS linux 2

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
+# installation for both ansible and packer and AWS linux 2
+sudo apt update -y
+
+# ansible
+sudo amazon-linux-extras install ansible2 -y
+
+# packer
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install packer
+
+# verify instillation
+ansible --version
+packer version
